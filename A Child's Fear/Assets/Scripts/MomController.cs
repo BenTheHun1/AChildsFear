1f2cfd5752c51fa401a99f70bfd7fb85fa73ec1a
BenTheHun1 <benhunsicker1@gmail.com>
2022-02-10 14:53:39 -0500

    AI work

---
+++ A Child's Fear/Assets/Scripts/MomController.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MomController : MonoBehaviour
+{
+    private Transform player;
+    private NavMeshAgent agent;
+    public float fov;
+
+    public Transform[] points;
+    private int destPoint = 0;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        player = GameObject.Find("Player").transform;
+
+        agent.autoBraking = false;
+
+        NextPoint();
+    }
+
+    void NextPoint()
+    {
+        Debug.Log(destPoint);
+        // Returns if no points have been set up
+        if (points.Length == 0)
+            return;
+
+        // Set the agent to go to the currently selected destination.
+        agent.destination = points[destPoint].position;
+
+        // Choose the next point in the array as the destination,
+        // cycling to the start if necessary.
+        destPoint = (destPoint + 1) % points.Length;
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(agent.remainingDistance);
+        Vector3 distanceToPlayer = player.position - transform.position;
+        float viewAngle = Vector3.Angle(distanceToPlayer, transform.forward);
+        RaycastHit hit;
+        if (viewAngle <= fov)
+        {
+            if (Physics.Raycast(transform.position, distanceToPlayer, out hit))
+            {
+                //Debug.Log(hit.transform.gameObject.name);
+                if (hit.transform.gameObject.name == "Player")
+                {
+                    agent.destination = player.position;
+                    Debug.Log("SPOTTED");
+                }
+            }
+        }
+        else if (!agent.pathPending && agent.remainingDistance < 0.5f)
+        {
+            NextPoint();
+        }
+
+        
+    }
+}
