ece87467671ab33d6715c95a9a275709e7b6d30a
BenTheHun1 <benhunsicker1@gmail.com>
2022-02-24 14:30:15 -0500

    Saving test moving platforms (dont use yet)

--- A Child's Fear/Assets/Scripts/MoveUpDown.cs
+++ A Child's Fear/Assets/Scripts/MoveUpDown.cs
@@ -4,18 +4,79 @@ using UnityEngine;
 
 public class MoveUpDown : MonoBehaviour
 {
-   public int[] MaxMin;
+    public axis inputAxis;
+    private float originalHeight; //Cannot be 0
+    public float inputNextValueOnAxis; //Cannot be 0
+    private float newPos;
+    private float nextHeight;
+    public float speed;
+    private float sensitivity = 0.1f;
+    
 
+    public enum axis {x, y, z}
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (inputAxis == axis.x)
+        {
+            originalHeight = gameObject.transform.position.x;
+        }
+        else if (inputAxis == axis.y)
+        {
+            originalHeight = gameObject.transform.position.y;
+        }
+        else
+        {
+            originalHeight = gameObject.transform.position.z;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        gameObject.transform.position = new Vector3 (gameObject.transform.position.x, Mathf.Lerp(MaxMin[0], MaxMin[1], 0.1f), gameObject.transform.position.z);
+        if (inputAxis == axis.x)
+        {
+            Debug.Log(gameObject.transform.position.x / inputNextValueOnAxis);
+            if (gameObject.transform.position.x / inputNextValueOnAxis <= 1 + sensitivity && gameObject.transform.position.x / inputNextValueOnAxis >= 1 - sensitivity)
+            {
+                nextHeight = originalHeight;
+            }
+            else if (gameObject.transform.position.x / originalHeight <= 1 + sensitivity && gameObject.transform.position.x / originalHeight >= 1 - sensitivity)
+            {
+                nextHeight = inputNextValueOnAxis;
+            }
+            newPos = Mathf.MoveTowards(gameObject.transform.position.x, nextHeight, speed * Time.deltaTime);
+            gameObject.transform.position = new Vector3(newPos, gameObject.transform.position.y, gameObject.transform.position.z);
+        }
+        else if (inputAxis == axis.y)
+        {
+            if (gameObject.transform.position.y / inputNextValueOnAxis <= 1 + sensitivity && gameObject.transform.position.y / inputNextValueOnAxis >= 1 - sensitivity)
+            {
+                nextHeight = originalHeight;
+            }
+            else if (gameObject.transform.position.y / originalHeight <= 1 + sensitivity && gameObject.transform.position.y / originalHeight >= 1 - sensitivity)
+            {
+                nextHeight = inputNextValueOnAxis;
+            }
+            newPos = Mathf.MoveTowards(gameObject.transform.position.y, nextHeight, speed * Time.deltaTime);
+            gameObject.transform.position = new Vector3(gameObject.transform.position.x, newPos, gameObject.transform.position.z);
+        }
+        else
+        {
+            if (gameObject.transform.position.z / inputNextValueOnAxis <= 1 + sensitivity && gameObject.transform.position.z / inputNextValueOnAxis >= 1 - sensitivity)
+            {
+                nextHeight = originalHeight;
+            }
+            else if (gameObject.transform.position.z / originalHeight <= 1 + sensitivity && gameObject.transform.position.z / originalHeight >= 1 - sensitivity)
+            {
+                nextHeight = inputNextValueOnAxis;
+            }
+            newPos = Mathf.MoveTowards(gameObject.transform.position.z, nextHeight, speed * Time.deltaTime);
+            gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, newPos);
+        }
+
+
+        //Debug.Log(gameObject.transform.position);
     }
 }
