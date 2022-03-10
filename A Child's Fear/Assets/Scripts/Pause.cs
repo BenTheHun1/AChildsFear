fafd8896e71b4f9e81c0fc2634b216242b166e31
Benjamin Hunsicker <benhunsicker1@gmail.com>
2022-03-10 14:35:58 -0500

    Pause menu

---
+++ A Child's Fear/Assets/Scripts/Pause.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour
+{
+    public GameObject pauseMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1;
+        pauseMenu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Time.timeScale == 1)
+            {
+                PauseGame();
+            }
+            else if (Time.timeScale == 0)
+            {
+                ResumeGame();
+            }
+        }
+    }
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+        pauseMenu.SetActive(true);
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1;
+        pauseMenu.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    public void LoadScene(string sceneToLoad)
+    {
+        if (sceneToLoad == "MomTest")
+        {
+            SceneManager.LoadScene("Main");
+        }
+        else
+        {
+            SceneManager.LoadScene("Working");
+        }
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
