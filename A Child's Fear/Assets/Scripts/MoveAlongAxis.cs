a30bb8d2d1b06f41caf5412df9f5f437951ae952
BenTheHun1 <benhunsicker1@gmail.com>
2022-03-31 15:14:37 -0400

    Revert "Reworking moving platforms :("
    This reverts commit a4fca47ee009a2d238cd8de9d0ac303d1be7a88e.

---
+++ A Child's Fear/Assets/Scripts/MoveAlongAxis.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveAlongAxis : MonoBehaviour
+{
+    public axis inputAxis;
+    private float originalHeight;
+    public float inputNextValueOnAxis;
+    private float newPos;
+    private float nextHeight;
+    public float speed;
+    
+    
+
+    public enum axis {x, y, z}
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (inputAxis == axis.x)
+        {
+            originalHeight = gameObject.transform.localPosition.x;
+        }
+        else if (inputAxis == axis.y)
+        {
+            originalHeight = gameObject.transform.localPosition.y;
+        }
+        else
+        {
+            originalHeight = gameObject.transform.localPosition.z;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inputAxis == axis.x)
+        {
+            if (gameObject.transform.localPosition.x == inputNextValueOnAxis)
+            {
+                nextHeight = originalHeight;
+            }
+            else if (gameObject.transform.localPosition.x == originalHeight)
+            {
+                nextHeight = inputNextValueOnAxis;
+            }
+            newPos = Mathf.MoveTowards(gameObject.transform.localPosition.x, nextHeight, speed * Time.deltaTime);
+            gameObject.transform.localPosition = new Vector3(newPos, gameObject.transform.localPosition.y, gameObject.transform.localPosition.z);
+        }
+        else if (inputAxis == axis.y)
+        {
+            if (gameObject.transform.localPosition.y == inputNextValueOnAxis)
+            {
+                nextHeight = originalHeight;
+            }
+            else if (gameObject.transform.localPosition.y == originalHeight)
+            {
+                nextHeight = inputNextValueOnAxis;
+            }
+            newPos = Mathf.MoveTowards(gameObject.transform.localPosition.y, nextHeight, speed * Time.deltaTime);
+            gameObject.transform.localPosition = new Vector3(gameObject.transform.localPosition.x, newPos, gameObject.transform.localPosition.z);
+        }
+        else
+        {
+            if (gameObject.transform.localPosition.z == inputNextValueOnAxis)
+            {
+                nextHeight = originalHeight;
+            }
+            else if (gameObject.transform.localPosition.z == originalHeight)
+            {
+                nextHeight = inputNextValueOnAxis;
+            }
+            newPos = Mathf.MoveTowards(gameObject.transform.localPosition.z, nextHeight, speed * Time.deltaTime);
+            gameObject.transform.localPosition = new Vector3(gameObject.transform.localPosition.x, gameObject.transform.localPosition.y, newPos);
+        }
+
+
+        //Debug.Log(gameObject.transform.position);
+    }
+}
