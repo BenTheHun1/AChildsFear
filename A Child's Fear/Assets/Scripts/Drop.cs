048a37478ec683d6d06c18c5e616ea2fbb2637c5
BenTheHun1 <benhunsicker1@gmail.com>
2022-04-28 13:46:58 -0400

    Added Dropping Platform Respawning

--- A Child's Fear/Assets/Scripts/Drop.cs
+++ A Child's Fear/Assets/Scripts/Drop.cs
@@ -10,15 +10,18 @@ public class Drop : MonoBehaviour
     public float fallSpeed;
     [Tooltip("Time in seconds to wait after the platform starts dropping to despawn it.")]
     public float despawnTime;
+    [Tooltip("Time in seconds to wait after the platform has despawned to respawn it.")]
+    public float respawnTime;
 
     [Tooltip("Is the platform currently dropping? Should be false on scene start.")]
     public bool isCurrentlyDropping;
     private float newPos;
+    private Vector3 startPos;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        startPos = gameObject.transform.position;
     }
 
     // Update is called once per frame
@@ -33,7 +36,10 @@ public class Drop : MonoBehaviour
                 despawnTime -= Time.deltaTime;
                 if (despawnTime <= 0f)
                 {
-                    Destroy(gameObject);
+                    gameObject.GetComponent<Collider>().enabled = false;
+                    gameObject.GetComponent<MeshRenderer>().enabled = false;
+                    isCurrentlyDropping = false;
+                    StartCoroutine("Respawn");
                 }
             }
             else
@@ -41,5 +47,13 @@ public class Drop : MonoBehaviour
                 waitTime -= Time.deltaTime;
             }
         }
+    }
+
+    IEnumerator Respawn()
+    {
+        yield return new WaitForSeconds(respawnTime);
+        gameObject.GetComponent<Collider>().enabled = true;
+        gameObject.GetComponent<MeshRenderer>().enabled = true;
+        gameObject.transform.position = startPos;
     }
 }
