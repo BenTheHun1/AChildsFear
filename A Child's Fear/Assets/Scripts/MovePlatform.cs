73ea7aa0a3e069425148f5650bb3cb3b4e2e3078
BenTheHun1 <benhunsicker1@gmail.com>
2022-04-28 14:37:41 -0400

    More instructions

--- A Child's Fear/Assets/Scripts/MovePlatform.cs
+++ A Child's Fear/Assets/Scripts/MovePlatform.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class MovePlatform : MonoBehaviour
 {
-    [Tooltip("Delete this Platform, add MovePlatform.cs to the new platform (which should be a child of PlatformKit and in the Ground Layer).\nMake sure it has a collider *and* a RIGIDBODY.\nChange the below settings as needed.")]
+    [Tooltip("Unpack Prefab completely (Right click->Prefab).\nDelete this Platform, add MovePlatform.cs to the new platform (which should be a child of PlatformKit and in the Ground Layer).\nMake sure it has a collider *and* a RIGIDBODY.\nChange the below settings as needed.")]
     public bool tutorial;
 
     [Tooltip("Speed platform Moves. Should be at least 1.")]
     public float speed;
-    [Tooltip("The Parent GameObject of all the transforms. Should have at least 2 children.")]
+    [Tooltip("The Parent GameObject of all the transforms.\n2+ moves between all of them, only 1 moves from starting position to there and then stops.")]
     public GameObject positionsParent; //Use at least 2 to move it
     [Tooltip("The index of the positionsParent representing the current destination. Should start at 0.")]
     public int currentPosition;
