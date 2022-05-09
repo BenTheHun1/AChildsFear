a8edfe8c6088decf0b86600cb46f7edb0ccbf09e
Benjamin Hunsicker <benhunsicker1@gmail.com>
2022-05-09 19:00:35 -0400

    Implemented slingshot anims

--- A Child's Fear/Assets/Scripts/slingshot.cs
+++ A Child's Fear/Assets/Scripts/slingshot.cs
@@ -10,11 +10,13 @@ public class slingshot : MonoBehaviour
     private GameObject newestBullet;
     public float force;
     private Text forceDisplay;
+    private Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
         forceDisplay = GameObject.Find("Slingshot Force").GetComponent<Text>();
+        anim = gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -23,12 +25,15 @@ public class slingshot : MonoBehaviour
         if (Input.GetKey(KeyCode.Mouse0) && force <= 5000)
         {
             force += 2000f * Time.deltaTime;
+            anim.SetBool("pull", true);
         }
         if (Input.GetKeyUp(KeyCode.Mouse0))
         {
+            anim.SetBool("pull", false);
+            anim.SetTrigger("release");
             if (force > 300)
             {
-                newestBullet = Instantiate(bullet, spawnpoint.position, transform.rotation);
+                newestBullet = Instantiate(bullet, spawnpoint.position, spawnpoint.rotation);
                 newestBullet.GetComponent<Rigidbody>().AddRelativeForce(Vector3.forward * force);
             }
             force = 0;
